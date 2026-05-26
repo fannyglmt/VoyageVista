103816a38ac0bee836556e88032021dc01d16a77
Sofia Salem <salem.sofia.2006@gmail.com>
2026-05-26 17:54:40 +0200

    Splash screen et amélioration accueil

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,13 @@
 console.log("VoyageVista chargé 🌴");
 
+window.addEventListener("load", () => {
+  const splash = document.getElementById("splash-screen");
+
+  setTimeout(() => {
+    splash.classList.add("hidden");
+  }, 7500);
+});
+
 const cards = document.querySelectorAll(".trend-card");
 
 cards.forEach((card) => {
