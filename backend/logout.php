7070ee95b214350a62c4893564ae8a4204f51223
mzd2549 <mariezduong@gmail.com>
2026-05-28 22:18:07 +0200

    ajout de la suppression des cookies dans logout.php

--- backend/logout.php
+++ backend/logout.php
@@ -2,6 +2,12 @@
 session_start();
 session_unset();
 session_destroy();
+
+// Supprime le cookie "remember me" si présent
+if (isset($_COOKIE['remember_token'])) {
+    setcookie('remember_token', '', time() - 3600, '/');
+}
+
 header("Location: ../frontend/index.html");
 exit;
 ?>
